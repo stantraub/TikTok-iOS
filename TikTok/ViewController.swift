6e85eb093370ea006f29fbebff4776e5bec86bad
Stanley <stanley.traub@gmail.com>
2021-05-04 10:16:08 -0700

    Setting up project

---
+++ TikTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Stanley Traub on 5/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
